a394b7f90265ef71fdb44b426e40105d69c9a32f
kamilego <kam.legowicz@gmail.com>
2021-03-07 00:45:31 +0100

    trying to rotate towers coordinates

--- antennas_gui.py
+++ antennas_gui.py
@@ -5,14 +5,14 @@ from math import *
 pyautogui.click()
 
 a1 = [66, 0.6]
-a2 = [346, 1.2]
+a2 = [346, 0.6]
 a3 = [212, 0.6]
 a4 = [120, 0.6]
 a5 = [310, 1.2]
 a6 = [20, 0.6]
 
-speed = 0.2
-
+speed = 0.5
+ant_azimuth = 60
 # to be changed end
 
 # towers locations
@@ -24,6 +24,13 @@ w2 = {"left": [1129, 352, 1121, 348],
       "right": [1293, 332, 1293, 337],
       "middle": [1228, 483, 1223, 489]}
 
+sP_w1_left = [714, 468]
+sP_w1_right = [990, 468]
+sP_w1_middle =[852, 231]
+
+sP_w2_left = [1079, 468]
+sP_w2_right = [1355, 468]
+sP_w2_middle =[1217, 231]
 # towers locations end
 
 azimuths = [a2, a3, a4, a5, a6]
@@ -48,8 +55,8 @@ def degree_change(azimuth):
         return degree
 
 
-def pozycja(azymut):
-    alfa = azymut - 90
+def position(azimuth):
+    alfa = azimuth - 90
     cos = math.cos(math.radians(alfa))
     sin = math.sin(math.radians(alfa))
     P0 = [1736, 882]
@@ -74,7 +81,7 @@ def rorate(a, v, lok):
     pyautogui.moveTo(x=1736, y=882, duration=speed)
     pyautogui.click()
     pyautogui.click(x=996, y=882, duration=speed)
-    pyautogui.click(pozycja(a), duration=speed)
+    pyautogui.click(position(a), duration=speed)
     pyautogui.moveTo(lok[0:2])
     pyautogui.scroll(1)
     pyautogui.scroll(1)
@@ -100,21 +107,122 @@ def retList():
 azimuth1, azimuth2 = retList()
 
 def check1(elem):
-    if 0 < elem < 119:
-         return w1["right"]
-    elif 118 < elem < 240:
-         return w1["middle"]
+    if 180 < elem < 300:
+         return pozcheck(sP_w1_left,pozicionen(w1["left"],ant_azimuth),pozicionen(w1["right"],ant_azimuth),pozicionen(w1["middle"],ant_azimuth))
+    elif 60 < elem < 180:
+         return pozcheck(sP_w1_right,pozicionen(w1["right"],ant_azimuth),pozicionen(w1["left"],ant_azimuth),pozicionen(w1["middle"],ant_azimuth))
     else:
-         return w1["left"]
+         return pozcheck(sP_w1_middle,pozicionen(w1["middle"],ant_azimuth),pozicionen(w1["left"],ant_azimuth),pozicionen(w1["right"],ant_azimuth))
 
 def check2(elem):
-     if 0 < elem < 119:
-        return w2["right"]
-     elif 118 < elem < 240:
-         return w2["middle"]
-     else:
-        return w2["left"]
+    if 180 < elem < 315:
+         return pozcheck(sP_w2_left,pozicionen(w2["left"],ant_azimuth),pozicionen(w2["right"],ant_azimuth),pozicionen(w2["middle"],ant_azimuth))
+    elif 60 < elem < 180:
+         return pozcheck(sP_w2_right,pozicionen(w2["right"],ant_azimuth),pozicionen(w2["left"],ant_azimuth),pozicionen(w2["middle"],ant_azimuth))
+    else:
+         return pozcheck(sP_w2_middle,pozicionen(w2["middle"],ant_azimuth),pozicionen(w2["left"],ant_azimuth),pozicionen(w2["right"],ant_azimuth))
 
+def pozicionen (elem,az):
+    if az < 120:
+        az = az + 0
+    if 120 < az < 240:
+        az = az - 120
+    if az > 240:
+        az = az - 240
+    if elem == w1["left"]:
+        alfa = az - 120 - 90
+        cos = math.cos(math.radians(alfa))
+        sin = math.sin(math.radians(alfa))
+        P0 = [851, 390]
+        P1 = elem[0:2]
+        P2 = elem[2:4]
+        l1 = sqrt((P0[0] - P1[0]) ** 2 + (P0[1] - P1[1]) ** 2)
+        l2 = sqrt((P0[0] - P2[0]) ** 2 + (P0[1] - P2[1]) ** 2)
+        x1 = P0[0] + l1 * cos
+        y1 = P0[1] + l1 * sin
+        x2 = P0[0] + l2 * cos
+        y2 = P0[1] + l2 * sin
+        return [x1, y1, x2, y2]
+    elif elem == w1["right"]:
+        alfa = az - 240 - 90
+        cos = math.cos(math.radians(alfa))
+        sin = math.sin(math.radians(alfa))
+        P0 = [851, 390]
+        P1 = elem[0:2]
+        P2 = elem[2:4]
+        l1 = sqrt((P0[0] - P1[0]) ** 2 + (P0[1] - P1[1]) ** 2)
+        l2 = sqrt((P0[0] - P2[0]) ** 2 + (P0[1] - P2[1]) ** 2)
+        x1 = P0[0] + l1 * cos
+        y1 = P0[1] + l1 * sin
+        x2 = P0[0] + l2 * cos
+        y2 = P0[1] + l2 * sin
+        return [x1, y1, x2, y2]
+    elif elem == w1["middle"]:
+        alfa = az - 90
+        cos = math.cos(math.radians(alfa))
+        sin = math.sin(math.radians(alfa))
+        P0 = [851, 390]
+        P1 = elem[0:2]
+        P2 = elem[2:4]
+        l1 = sqrt((P0[0] - P1[0]) ** 2 + (P0[1] - P1[1]) ** 2)
+        l2 = sqrt((P0[0] - P2[0]) ** 2 + (P0[1] - P2[1]) ** 2)
+        x1 = P0[0] + l1 * cos
+        y1 = P0[1] + l1 * sin
+        x2 = P0[0] + l2 * cos
+        y2 = P0[1] + l2 * sin
+        return [x1, y1, x2, y2]
+    elif elem == w2["left"]:
+        alfa = az - 120 - 90
+        cos = math.cos(math.radians(alfa))
+        sin = math.sin(math.radians(alfa))
+        P0 = [1216, 390]
+        P1 = elem[0:2]
+        P2 = elem[2:4]
+        l1 = sqrt((P0[0] - P1[0]) ** 2 + (P0[1] - P1[1]) ** 2)
+        l2 = sqrt((P0[0] - P2[0]) ** 2 + (P0[1] - P2[1]) ** 2)
+        x1 = P0[0] + l1 * cos
+        y1 = P0[1] + l1 * sin
+        x2 = P0[0] + l2 * cos
+        y2 = P0[1] + l2 * sin
+        return [x1, y1, x2, y2]
+    elif elem == w2["right"]:
+        alfa = az - 240 - 90
+        cos = math.cos(math.radians(alfa))
+        sin = math.sin(math.radians(alfa))
+        P0 = [1216, 390]
+        P1 = elem[0:2]
+        P2 = elem[2:4]
+        l1 = sqrt((P0[0] - P1[0]) ** 2 + (P0[1] - P1[1]) ** 2)
+        l2 = sqrt((P0[0] - P2[0]) ** 2 + (P0[1] - P2[1]) ** 2)
+        x1 = P0[0] + l1 * cos
+        y1 = P0[1] + l1 * sin
+        x2 = P0[0] + l2 * cos
+        y2 = P0[1] + l2 * sin
+        return [x1, y1, x2, y2]
+    elif elem == w2["middle"]:
+        alfa = az - 90
+        cos = math.cos(math.radians(alfa))
+        sin = math.sin(math.radians(alfa))
+        P0 = [1216, 390]
+        P1 = elem[0:2]
+        P2 = elem[2:4]
+        l1 = sqrt((P0[0] - P1[0]) ** 2 + (P0[1] - P1[1]) ** 2)
+        l2 = sqrt((P0[0] - P2[0]) ** 2 + (P0[1] - P2[1]) ** 2)
+        x1 = P0[0] + l1 * cos
+        y1 = P0[1] + l1 * sin
+        x2 = P0[0] + l2 * cos
+        y2 = P0[1] + l2 * sin
+        return [x1, y1, x2, y2]
+
+
+
+def pozcheck(sP,wclose,wfar1,wfar2):
+    l1 = int(sqrt((sP[0] - wclose[0]) ** 2 + (sP[1] - wclose[1]) ** 2))
+    l2 = int(sqrt((sP[0] - wfar1[0]) ** 2 + (sP[1] - wfar1[1]) ** 2))
+    l3 = int(sqrt((sP[0] - wfar2[0]) ** 2 + (sP[1] - wfar2[1]) ** 2))
+    lista = sorted([l1, l3, l2])
+    if lista[0] == l1:
+        return wclose
 
 
 rorate(azimuth1[0][0],azimuth1[0][1],check1(azimuth1[0][0]))
